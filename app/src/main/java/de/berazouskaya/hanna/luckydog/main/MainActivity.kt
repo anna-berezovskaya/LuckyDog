b8fdfb88698451d5d422482c4fa90c9671e8dcfa
hanna <anna.berzovskaya@e-therapists.de>
2019-01-07 12:00:30 +0100

    main activity and layout file

---
+++ app/src/main/java/de/berazouskaya/hanna/luckydog/main/MainActivity.kt
@@ -0,0 +1,13 @@
+package de.berazouskaya.hanna.luckydog.main
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import de.berazouskaya.hanna.luckydog.R
+
+class MainActivity : AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+    }
+}
